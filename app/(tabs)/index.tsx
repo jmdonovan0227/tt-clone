d2c031d4a4629809e02b20673999f72fe3178ac0
Jake Donovan <jakedonovan0227@Jakes-Mac-mini.local>
2025-12-01 14:36:41 -0500

    Shows an example video that takes up screen space, shows buttons for liking, sharing, commenting, and placeholder profile button and a username txt with a video description

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
+import PostListItem from "@/components/PostListItem";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <PostListItem />
     </View>
   );
 }
