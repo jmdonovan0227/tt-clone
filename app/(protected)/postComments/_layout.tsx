a15be69c18fa495c5c051864f7d68eb7cb9de965
Jake Donovan <jakedonovan0227@Jakes-Mac-mini.local>
2025-12-16 22:27:49 -0500

    Added sharing videos feature, updated comments page styling with a new back button, fixed realtime bug showing new comments count when one is added

--- app/(protected)/postComments/_layout.tsx
+++ app/(protected)/postComments/_layout.tsx
@@ -1,14 +1,45 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity, View, Text } from "react-native";
 
 export default function PostCommentsLayout() {
+  const router = useRouter();
+
   return (
     <Stack>
       <Stack.Screen
         name="[id]"
         options={{
           headerShown: true,
-          title: "Comments",
-          headerBackButtonDisplayMode: "minimal",
+          headerBackButtonDisplayMode: "default",
+          headerStyle: {
+            backgroundColor: "black",
+          },
+
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerTitle: () => (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{ color: "white", fontWeight: "bold", fontSize: 20 }}
+              >
+                Comments
+              </Text>
+            </View>
+          ),
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="arrow-back" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+          headerBackVisible: false,
         }}
       />
       <Stack.Screen
